8b44d61042960fdb6a91a8547014c4e93d87a39f
RinkuNahar <rinkunaharhossan2022@gmail.com>
2022-04-04 14:11:07 +0600

    not found page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Heading from './components/Heading/Heading';
 import HomePage from './components/HomePage/HomePage';
+import NotFound from './components/NotFound/NotFound';
 import Review from './components/Review/Review';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage></HomePage>}></Route>
           <Route path='/reviews' element={<Review></Review>}></Route>
+          <Route path='*' element={<NotFound></NotFound>}></Route>
         </Routes>
     </div>
   );
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NotFound.css'
+const NotFound = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <h2>Not Found This Page</h2>
+        </div>
+    );
+};
+
+export default NotFound;
